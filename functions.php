345d63b32f6063803d2ec73871f8aa6588ad0110
LAPTOP-D23MA2SP\ryosuke <ryosuke9913@gmail.com>
2018-11-02 18:55:07 +0900

    サイドバーの追加

--- functions.php
+++ functions.php
@@ -2,4 +2,25 @@
 //アイキャッチ画像の有効化
  
 add_theme_support('post-thumbnails');
+
+if ( function_exists('register_sidebar') ){
+ 
+register_sidebar(array(
+ 
+ 'name' => 'サイドバー',
+ 
+ 'id' => 'sidebar',
+ 
+ 'description' => 'ウィジェットの説明',
+ 
+ 'before_widget' => '<div>',
+ 
+ 'after_widget' => '</div>',
+ 
+ 'before_title' => '<h3>',
+ 
+ 'after_title' => '</h3>'
+ 
+ ));
+}
 ?>
--- index.php
+++ index.php
@@ -9,74 +9,76 @@
     
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/PostedList.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/sidebar.css" type="text/css" />
 </head>
     <body>
-        <?php// get_header(); ?>
-        <div class="topic-container">
-        <?php
-            $example_posts = get_posts('cat="ほげ"post_type=post&posts_per_page=3');  
-            ?>
-        <?php
-        foreach ( $example_posts as $post ):
-            setup_postdata($post);?>
-            <div class="topic-post">
-                <div class="pick-up"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/new.png" width="70" height="70"></div>
-                <div class="post-image"><a href="<?php the_permalink(); ?>">
-                <?php if (has_post_thumbnail()): ?>
-                <?php the_post_thumbnail(array(100, 100)); ?>
-                <?php else: ?>
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/noimage.png" width="100" height="100">
-                <?php endif; ?>
-                </a></div>
-                <h3>
-                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                </h3>
-                <div class="post-meta">
-                    <div class="category"><?php the_category(' '); ?></div>
-                    <div class="data-time"><?php echo get_the_date(); ?></div>
-                </div>
-            </div>
-            <?php endforeach;
-            wp_reset_postdata(); ?>
-        </div>
-        <div class="container">
+        <?php get_sidebar(); ?>
+        <div class="main">
+            <?php// get_header(); ?>   
+            <div class="topic-container">
             <?php
-            if(have_posts()):
-                while (have_posts()):
-                    the_post();
+                $example_posts = get_posts('cat="ほげ"post_type=post&posts_per_page=3');  
                 ?>
-            <div class="post">
-                
-                <div class="post-image"><a href="<?php the_permalink(); ?>">
-                <?php if (has_post_thumbnail()): ?>
-                <?php the_post_thumbnail(array(100, 100)); ?>
-                <?php else: ?>
-                        <img src="<?php echo get_template_directory_uri(); ?>/img/noimage.png" width="100" height="100">
-                <?php endif; ?>
-                </a></div>
-                <h3>
-                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                </h3>
-                <div class="post-meta">
-                    <div class="category"><?php the_category(' '); ?></div>
-                    <div class="data-time"><?php echo get_the_date(); ?></div>
-                </div>
-            </div>
             <?php
-            endwhile;
-            else:
-        ?>
-            <p>記事はありません！</p>
-        <?php endif; ?>
-            
-            <?php //get_sidebar(); ?>
-        </div><!-- /main -->
-        <?php the_posts_pagination(array(
-            'prev_text' => '◀',
-            'mid_size'  => 10,
-            'next_text' => '▶',
-            )); 
-        ?>
-        <?//php get_footer(); ?> 
+            foreach ( $example_posts as $post ):
+                setup_postdata($post);?>
+                <div class="topic-post">
+                    <div class="pick-up"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/new.png" width="70" height="70"></div>
+                    <div class="post-image"><a href="<?php the_permalink(); ?>">
+                    <?php if (has_post_thumbnail()): ?>
+                    <?php the_post_thumbnail(array(100, 100)); ?>
+                    <?php else: ?>
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/noimage.png" width="100" height="100">
+                    <?php endif; ?>
+                    </a></div>
+                    <h3>
+                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    </h3>
+                    <div class="post-meta">
+                        <div class="category"><?php the_category(' '); ?></div>
+                        <div class="data-time"><?php echo get_the_date(); ?></div>
+                    </div>
+                </div>
+                <?php endforeach;
+                wp_reset_postdata(); ?>
+            </div>
+            <div class="container">
+                <?php
+                if(have_posts()):
+                    while (have_posts()):
+                        the_post();
+                    ?>
+                <div class="post">
+                    
+                    <div class="post-image"><a href="<?php the_permalink(); ?>">
+                    <?php if (has_post_thumbnail()): ?>
+                    <?php the_post_thumbnail(array(100, 100)); ?>
+                    <?php else: ?>
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/noimage.png" width="100" height="100">
+                    <?php endif; ?>
+                    </a></div>
+                    <h3>
+                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    </h3>
+                    <div class="post-meta">
+                        <div class="category"><?php the_category(' '); ?></div>
+                        <div class="data-time"><?php echo get_the_date(); ?></div>
+                    </div>
+                </div>
+                <?php
+                endwhile;
+                else:
+            ?>
+                <p>記事はありません！</p>
+            <?php endif; ?>
+            </div><!-- /main -->
+            <?php the_posts_pagination(array(
+                'prev_text' => '◀',
+                'mid_size'  => 10,
+                'next_text' => '▶',
+                )); 
+            ?>
+            <?//php get_footer(); ?> 
+        </div>
     </body>
 </html>
