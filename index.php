0366e5a238443f9d26dc2c304b7561c3b2b81e88
ryosuke <ryosuke9913@gmail.com>
2018-12-27 22:23:23 +0900

    記事にアイキャッチがないときの処理

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
                 <?php if (has_post_thumbnail()): ?>
                 <?php the_post_thumbnail(array(100, 100)); ?>
                 <?php else: ?>
-                        <img src="<?php echo get_template_directory_uri(); ?>/img/noimage.png" width="100" height="100">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/noimage.png" width="100" height="100">
                 <?php endif; ?>
                 </a></div>
                 <h3>
