1959424b19aeac530d02b75d47f404a81755cabd
AnQ_tpu <mskz625@gmail.com>
2019-03-21 13:43:03 +0900

    tpumarkerとは？のページの作成

---
+++ recruitment-wanted.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TPUMarker</title>
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel='stylesheet' href='https://unpkg.com/ress/dist/ress.min.css'>
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/css/style.min.css">
+    <link rel="stylesheet" href="./assets/css/tpumarker.min.css">
+  </head>
+  <body>
+    <div class="contents">
+      <div class="sidebar">
+        <nav class="content">
+          <div class="logo">
+            <a href="./index.html">
+              <img src="./assets/img/design/MarkerLogo.png" alt="">
+            </a>
+          </div>
+          <div class="contents-list">
+            <ul>
+              <li class="univ">
+                <div style="border-left: 10px solid #c7243a" class="category-ribbon"></div>
+                <div class="category-chevron"></div>
+                <a href="#">大学</a>
+              </li>
+              <li class="student">
+                <div style="border-left: 10px solid #3a24c7" class="category-ribbon"></div>
+                <div class="category-chevron"></div>
+                <a href="#">学生</a>
+              </li>
+              <li class="region">
+                <div style="border-left: 10px solid #3ac724" class="category-ribbon"></div>
+                <div class="category-chevron"></div>
+                <a href="#">地域</a>
+              </li>
+            </ul>
+          </div>
+          <div class="lecture-cancellation">
+            <img src="assets/img/design/kyuko.svg" alt="" width="">
+            <div class="lecture">
+              <ul>
+                <li class="lecture-date">
+                  <h2>2月1日</h2>
+                  <ul>
+                    <li class="lecture-data">13:10 ~ 14:40 岩本先生 〇〇工学</li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="assistance">
+            <img src="assets/img/design/hoko.svg" alt="" width="">
+            <div class="lecture">
+              <ul>
+                <li class="lecture-date">
+                  <h2>2月7日</h2>
+                  <ul>
+                    <li class="lecture-data">13:10 ~ 14:40 岩本先生 〇〇工学</li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="recruitment">
+            <button type="button" name="button">
+              <a href="recruitment-wanted.html">記者募集</a>
+            </button>
+          </div>
+        </nav>
+      </div>
+      <div class="main-contents">
+        <div class="main-content">
+          <div class="content">
+            <h2>TPUmarker記者募集中!!</h2>
+            <p>TPUmarkerの記者を募集しています。「情報を発信することが好き」、「写真を撮るのが好き」、「一緒に活動する仲間を集めたい」 「サークルやイベントの情報を発信したい」という方にオススメです。現在、約10名が記者として様々な情報を発信しています。 </p>
+            <p>お問い合わせメールアドレス：tpuccm@gmail.com</p>
+          </div>
+          <div class="content">
+            <h2>エンジニア募集中!!</h2>
+            <p>webサイトを運営するエンジニアも募集中です。知識や実力がなくてもwebに興味がある方なら誰でもokです。 初心者や経験者でも、その人に見合った方法でwebの勉強を行い、TPUmarkerの運営に携わってもらいます。 </p>
+            <p>また、月に２回デザイン勉強会を行っています。デザイン勉強会は誰でも参加可能です。開催時期が近くなるとサイト内で告知しますので 随時チェックをお願いします。</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <?php get_footer(); ?>
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script src="./assets/js/search-window.js"></script>
+  </body>
+</html>
