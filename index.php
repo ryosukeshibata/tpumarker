c711f722a953c50252a98f037ff318e25c116560
AnQ_tpu <mskz625@gmail.com>
2019-03-21 13:38:39 +0900

    index.phpのデザイン修正

--- index.php
+++ index.php
@@ -10,74 +10,52 @@
         <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
         <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.min.css" type="text/css" />
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/index.min.css" type="text/css" />
     </head>
     <body>
-        <?php get_sidebar(); ?>
-        <div class="main">
-            <?php// get_header(); ?>   
-            <div class="topic-container">
-            <?php
-                $example_posts = get_posts('cat="ほげ"post_type=post&posts_per_page=3');  
-                ?>
-            <?php
-            foreach ( $example_posts as $post ):
-                setup_postdata($post);?>
-                <div class="topic-post">
-                    <div class="pick-up"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/new.png" width="70" height="70"></div>
-                    <div class="post-image"><a href="<?php the_permalink(); ?>">
-                    <?php if (has_post_thumbnail()): ?>
-                    <?php the_post_thumbnail(array(100, 100)); ?>
-                    <?php else: ?>
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/noimage.png" width="100" height="100">
-                    <?php endif; ?>
-                    </a></div>
-                    <h3>
-                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                    </h3>
-                    <div class="post-meta">
-                        <div class="category"><?php the_category(' '); ?></div>
-                        <div class="data-time"><?php echo get_the_date(); ?></div>
-                    </div>
-                </div>
-                <?php endforeach;
-                wp_reset_postdata(); ?>
-            </div>
-            <div class="container">
-                <?php
-                if(have_posts()):
-                    while (have_posts()):
-                        the_post();
         <div class="contents">
             <?php get_sidebar(); ?>
+            <div class="topics">
+                <ul class="topic-contents">
+                    <?php
+                    if(have_posts()):
+                        while (have_posts()):
+                            the_post();
                     ?>
-                <div class="post">
-                    
-                    <div class="post-image"><a href="<?php the_permalink(); ?>">
-                    <?php if (has_post_thumbnail()): ?>
-                    <?php the_post_thumbnail(array(100, 100)); ?>
-                    <?php else: ?>
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/noimage.png" width="100" height="100">
+                            <li class="topic-content">
+                                <div class="topic-data">
+                                    <?php
+                                    if (has_post_thumbnail()):
+                                    ?>
+                                        <?php the_post_thumbnail(array(100, 100)); ?>
+                                    <?php
+                                    else:
+                                    ?>
+                                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/thumbnails/empty.png" alt="" class="post-thumbnail">
+                                    <?php endif; ?>
+                                    <div class="post-title"><?php the_title(); ?></div>
+                                    <div class="post-excerpt"><?php the_content('More ...'); ?></div>
+                                    <div class="post-date"><?php echo get_the_date(); ?></div>
+                                </div>
+                                <a href="<?php the_permalink() ?>" class="post-link"></a>
+                                <ul class="post-categories">
+                                    <li class="post-category">
+                                        <?php the_category(' '); ?>
+                                    </li>
+                                </ul>
+                            </li>
+                    <?php
+                        endwhile;
+                    else:
+                    ?>
+                        <p class="no-content">記事はありません！</p>
                     <?php endif; ?>
-                    </a></div>
-                    <h3>
-                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                    </h3>
-                    <div class="post-meta">
-                        <div class="category"><?php the_category(' '); ?></div>
-                        <div class="data-time"><?php echo get_the_date(); ?></div>
-                    </div>
-                </div>
+                </ul>
+
                 <?php
-                endwhile;
-                else:
-            ?>
-                <p>記事はありません！</p>
-            <?php endif; ?>
-            </div><!-- /main -->
-            <?php 
-                wpbeginner_numeric_posts_nav();
-            ?>
-            <?//php get_footer(); ?> 
+                    wpbeginner_numeric_posts_nav();
+                ?>
+            </div>
         </div>
         <?php get_footer(); ?>
     </body>
