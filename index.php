e724a3b6e49ea88140ad0f9feaebf87e13414492
AnQ_tpu <mskz625@gmail.com>
2019-03-21 13:37:37 +0900

    sidebarの修正

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
-<html>
+<html lang="ja">
     <head>
-        <meta charset="utf-8" />
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>TPUmarker</title>
         <meta name="TPUmarker" content="">
+        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+        <link rel='stylesheet' href='https://unpkg.com/ress/dist/ress.min.css'>
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
-        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/PostedList.css" type="text/css" />
-        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/sidebar.css" type="text/css" />
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.min.css" type="text/css" />
     </head>
     <body>
         <?php get_sidebar(); ?>
@@ -44,6 +47,8 @@
                 if(have_posts()):
                     while (have_posts()):
                         the_post();
+        <div class="contents">
+            <?php get_sidebar(); ?>
                     ?>
                 <div class="post">
                     
--- sidebar.php
+++ sidebar.php
@@ -1,32 +1,68 @@
 <div class="sidebar">
-  <!-- <?php dynamic_sidebar(sidebar);?> -->
-  <div class="">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/ccm.png" width="100" height="100">
-    <form action="" name="searchForm">
-      <div class="searchFrame">
-        <input type="text" id="search" name="searchBox" value="" class="inputBox"/>
-        <div class="clearButton" onclick="ClearButton_Click()"></div>
-      </div>
-    </form>
-    <div class="">
-      <button type="button" onclick="location.href=''">大学</button>
-    </div>
-    <div class="">
-      <button type="button" onclick="location.href=''">学生</button>
-    </div>
-    <div class="">
-      <button type="button" onclick="location.href=''">地域</button>
-    </div>
-  </div>
-  <div class="">
-    <div class="">
-      休講講義
-    </div>
-    <div class="">
-      補講講義
-    </div>
-  </div>
-  <div class="">
-    <button type="button" name="button">Twitter</button>
-  </div>
-</div><!-- /sidebar -->
+    <nav class="content">
+        <div class="logo">
+            <a href="./index.html">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/design/MarkerLogo.png" alt="">
+            </a>
+        </div>
+        <div class="search-window">
+            <form method="get" class="searchform clearfix" action="#">
+                <div class="parts-search">
+                    <input type="text" value="" class="search-input" />
+                    <input type="submit" class="search-submit" value="&#xf002;" />
+                </div>
+            </form>
+        </div>
+        <div class="contents-list">
+            <ul>
+                <li class="univ">
+                    <div style="border-left: 10px solid #c7243a" class="category-ribbon"></div>
+                    <div class="category-chevron"></div>
+                    <a href="./?cat=2">大学</a>
+                </li>
+                <li class="student">
+                    <div style="border-left: 10px solid #3a24c7" class="category-ribbon"></div>
+                    <div class="category-chevron"></div>
+                    <a href="./?cat=3">学生</a>
+                </li>
+                <li class="region">
+                    <div style="border-left: 10px solid #3ac724" class="category-ribbon"></div>
+                    <div class="category-chevron"></div>
+                    <a href="./?cat=4">地域</a>
+                </li>
+            </ul>
+        </div>
+        <div class="lecture-cancellation">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/design/kyuko.svg" alt="" width="">
+            <div class="lecture">
+                <ul>
+                    <li class="lecture-date">
+                        <h2>2月1日</h2>
+                        <ul>
+                            <li class="lecture-data">13:10 ~ 14:40 岩本先生 〇〇工学</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="assistance">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/design/hoko.svg" alt="" width="">
+            <div class="lecture">
+                <ul>
+                    <li class="lecture-date">
+                        <h2>2月7日</h2>
+                        <ul>
+                            <li class="lecture-data">13:10 ~ 14:40 岩本先生 〇〇工学</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="recruitment">
+            <button type="button" name="button">
+                <a href="recruitment-wanted.html">記者募集</a>
+            </button>
+        </div>
+    </nav>
+</div>
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/js/search-window.js"></script>
