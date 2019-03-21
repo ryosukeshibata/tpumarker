5a070557baad8891daa6b88578527bcd4d8746fe
AnQ_tpu <mskz625@gmail.com>
2019-03-21 13:43:30 +0900

    tpumarkerとは？ページの作成

---
+++ tpumarker.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TPUMarker</title>
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel='stylesheet' href='https://unpkg.com/ress/dist/ress.min.css'>
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/css/style.min.css">
+    <link rel="stylesheet" href="./assets/css/tpumarker.min.css">
+</head>
+<body>
+    <div class="contents">
+        <?php get_sidebar(); ?>
+        <div class="main-contents">
+            <div class="main-content">
+                <div class="content">
+                    <h2>TPUmarker記者募集中!!</h2>
+                    <p>TPUmarkerの記者を募集しています。「情報を発信することが好き」、「写真を撮るのが好き」、「一緒に活動する仲間を集めたい」 「サークルやイベントの情報を発信したい」という方にオススメです。現在、約10名が記者として様々な情報を発信しています。 </p>
+                    <p>お問い合わせメールアドレス：tpuccm@gmail.com</p>
+                </div>
+                <div class="content">
+                    <h2>エンジニア募集中!!</h2>
+                    <p>webサイトを運営するエンジニアも募集中です。知識や実力がなくてもwebに興味がある方なら誰でもokです。 初心者や経験者でも、その人に見合った方法でwebの勉強を行い、TPUmarkerの運営に携わってもらいます。 </p>
+                    <p>また、月に２回デザイン勉強会を行っています。デザイン勉強会は誰でも参加可能です。開催時期が近くなるとサイト内で告知しますので 随時チェックをお願いします。</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php get_footer(); ?>
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script src="./assets/js/search-window.js"></script>
+  </body>
+</html>
