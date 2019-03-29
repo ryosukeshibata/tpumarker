4c304ed8a676a789f34f36a9ee5cd76f047d7dfa
AnQ_tpu <mskz625@gmail.com>
2019-03-29 17:50:35 +0900

    コンテンツの修正

--- recruitment-wanted.php
+++ recruitment-wanted.php
@@ -1,3 +1,8 @@
+<?php
+/*
+Template Name:recruitment-wanted
+*/
+?>
 <!DOCTYPE html>
 <html lang="ja">
   <head>
@@ -7,76 +12,20 @@
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel='stylesheet' href='https://unpkg.com/ress/dist/ress.min.css'>
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-    <link rel="stylesheet" href="./assets/css/style.min.css">
-    <link rel="stylesheet" href="./assets/css/tpumarker.min.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.min.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/tpumarker.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
   </head>
   <body>
     <div class="contents">
-      <div class="sidebar">
-        <nav class="content">
-          <div class="logo">
-            <a href="./index.html">
-              <img src="./assets/img/design/MarkerLogo.png" alt="">
-            </a>
-          </div>
-          <div class="contents-list">
-            <ul>
-              <li class="univ">
-                <div style="border-left: 10px solid #c7243a" class="category-ribbon"></div>
-                <div class="category-chevron"></div>
-                <a href="#">大学</a>
-              </li>
-              <li class="student">
-                <div style="border-left: 10px solid #3a24c7" class="category-ribbon"></div>
-                <div class="category-chevron"></div>
-                <a href="#">学生</a>
-              </li>
-              <li class="region">
-                <div style="border-left: 10px solid #3ac724" class="category-ribbon"></div>
-                <div class="category-chevron"></div>
-                <a href="#">地域</a>
-              </li>
-            </ul>
-          </div>
-          <div class="lecture-cancellation">
-            <img src="assets/img/design/kyuko.svg" alt="" width="">
-            <div class="lecture">
-              <ul>
-                <li class="lecture-date">
-                  <h2>2月1日</h2>
-                  <ul>
-                    <li class="lecture-data">13:10 ~ 14:40 岩本先生 〇〇工学</li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div class="assistance">
-            <img src="assets/img/design/hoko.svg" alt="" width="">
-            <div class="lecture">
-              <ul>
-                <li class="lecture-date">
-                  <h2>2月7日</h2>
-                  <ul>
-                    <li class="lecture-data">13:10 ~ 14:40 岩本先生 〇〇工学</li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div class="recruitment">
-            <button type="button" name="button">
-              <a href="recruitment-wanted.html">記者募集</a>
-            </button>
-          </div>
-        </nav>
-      </div>
+        <?php get_sidebar(); ?>
       <div class="main-contents">
         <div class="main-content">
           <div class="content">
             <h2>TPUmarker記者募集中!!</h2>
             <p>TPUmarkerの記者を募集しています。「情報を発信することが好き」、「写真を撮るのが好き」、「一緒に活動する仲間を集めたい」 「サークルやイベントの情報を発信したい」という方にオススメです。現在、約10名が記者として様々な情報を発信しています。 </p>
-            <p>お問い合わせメールアドレス：tpuccm@gmail.com</p>
+            <p>お問い合わせメールアドレス：tpuccm[at]gmail.com</p>
+            <p>*[at]を@に書き換えてください</p>
           </div>
           <div class="content">
             <h2>エンジニア募集中!!</h2>
@@ -87,7 +36,5 @@
       </div>
     </div>
     <?php get_footer(); ?>
-    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-    <script src="./assets/js/search-window.js"></script>
   </body>
 </html>
--- tpumarker.php
+++ tpumarker.php
@@ -1,3 +1,8 @@
+<?php
+/*
+Template Name:tpumarker
+*/
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -7,8 +12,9 @@
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel='stylesheet' href='https://unpkg.com/ress/dist/ress.min.css'>
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-    <link rel="stylesheet" href="./assets/css/style.min.css">
-    <link rel="stylesheet" href="./assets/css/tpumarker.min.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.min.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/tpumarker.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 </head>
 <body>
     <div class="contents">
@@ -16,20 +22,48 @@
         <div class="main-contents">
             <div class="main-content">
                 <div class="content">
-                    <h2>TPUmarker記者募集中!!</h2>
-                    <p>TPUmarkerの記者を募集しています。「情報を発信することが好き」、「写真を撮るのが好き」、「一緒に活動する仲間を集めたい」 「サークルやイベントの情報を発信したい」という方にオススメです。現在、約10名が記者として様々な情報を発信しています。 </p>
-                    <p>お問い合わせメールアドレス：tpuccm@gmail.com</p>
+                    <h2>TPUMarkerとは？</h2>
+                    <p>TPU markerは富山県立大学（以下、県大）の学生が県大やその周辺の出来事を学生目線で記事にし、紹介するニュースサイトです。現在は、学生団体Core Creative Managerが運営を行っています。大学事務局などは運営に関わっておりません。</p>
+                    <p>お問い合わせメールアドレス：tpuccm[at]gmail.com</p>
+                    <p>*[at]を@に書き換えてください</p>
                 </div>
                 <div class="content">
-                    <h2>エンジニア募集中!!</h2>
-                    <p>webサイトを運営するエンジニアも募集中です。知識や実力がなくてもwebに興味がある方なら誰でもokです。 初心者や経験者でも、その人に見合った方法でwebの勉強を行い、TPUmarkerの運営に携わってもらいます。 </p>
-                    <p>また、月に２回デザイン勉強会を行っています。デザイン勉強会は誰でも参加可能です。開催時期が近くなるとサイト内で告知しますので 随時チェックをお願いします。</p>
+                    <h2>Core Creative Managerとは？</h2>
+                    <p>Core Creative ManagerはWebやアプリの開発を通して富山県立大学をより良い大学にすることを目的に活動している学生団体です。工学部らしい「工学心」を持った大学貢献を行っています。</p>
+                </div>
+                <div class="content">
+                    <h2>Core Creative Managerの活動内容</h2>
+                    <p>・Webサイト「TPU marker」の開発、運営</p>
+                    <p>・キャリアセンターのWeb開発</p>
+                    <p>・掲示板の電子化</p>
+                </div>
+                <div class="content">
+                    <h2>TPU markerの目的</h2>
+                    <div class="content-detail">
+                        <h3>・大学の情報共有</h3>
+                        <p>日頃から県大ではみなさんに知られていない多くの出来事が起きています。例えば、あるサークルが大会で賞を貰ったり、学科のある教員が昇進されたり、 また、県大の一角に突如かまくらが作られるという怪事件が起きたり、様々な出来事が日々起きています。</p>
+                        <p>TPU markerではそのままでは埋もれてしまう大学の様々な出来事をニュースとし、学生自らが記者となって見える化することで、多くの学生のためになる県大の情報を共有していきたいと考えています。</p>
+                    </div>
+                    <div class="content-detail">
+                        <h3>・学生による大学活性化</h3>
+                        <p>TPU markerは自主的でチャレンジ精神のある学生を応援しています。TPU markerを通して、彼らの活動をお多くの学生に知っていただき、モチベーションの維持や仲間集めに役立ててもらいたいと考えています。加えて、多くの学生に自主的でチャレンジングな活動を知らせることで、大学活性化を行うことができると考えています。しかし、自主的な活動と言っても、多くの学生は具体的なイメージを持つのは難しく、また、したい活動があったとしても、どのように始めればいいのかイメージできません。TPU markerでは、これから新たに活動を行う際のヒントを提供することができると考えています。TPU markerでは学生の自主的な活動やそれをサポートする大学の試みに積極的に取材を行い、学生による大学活性化の力になれるよう努力します。</p>
+                    </div>
+                    <div class="content-detail">
+                        <h3>・大学の広報</h3>
+                        <p>TPU markerは県大生同士の情報共有だけでなく、情報を外部に公開することで大学の広報に寄与することも目指します。大学進学を視野に入れている高校生や富山県の人々、また、他大学様、企業様など多くの人々に情報を発信するメディアとしての役割を担います。学生による「県大の今」の情報発信によって、高校生にとってはネット上のオープンキャンパスとしての活用を、企業様や地域には県大が今何を目指しているのか知っていただく機会作りを目指します。</p>
+                    </div>
+                    <div class="content-detail">
+                        <h3>・地域貢献</h3>
+                        <p>富山県立大学は「地（知）の拠点〜Center Of Community（COC）〜」整備事業として、地域と連携し地域に貢献する大学を目指しています。その方針はこのTPU markerも同様です。大学内だけでなく大学周辺、また富山県についての情報も学生記者による独自取材により発信していきます。県大において、大学を広報することは、富山県のプロデュースでもあるのです。「富山県立大学のある富山県とはどんなところなのか」「私達、県大生はどんな地域で活躍していくのか」といったことを発信するメディアを目指しています。</p>
+                    </div>
+                </div>
+                <div class="content">
+                    <h2>コンテンツの取り扱い規約</h2>
+                    <p>TPU marker編集部によって提供されたコンテンツ(画像・イラスト等)の著作権は、TPU marker編集部、ないしはコンテンツの提供元に帰属します。したがって、著作権法で認められている場合を除いて、TPU marker編集部の事前の承諾なしに、コンテンツの転載、複製等の利用はできません。また、記事の転載に関しましては、引用元がわかるように、明記した上で行っていただくようお願いします。</p>
                 </div>
             </div>
         </div>
     </div>
     <?php get_footer(); ?>
-    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-    <script src="./assets/js/search-window.js"></script>
   </body>
 </html>
